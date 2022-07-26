362f2d7559048ea2d60eb5f430ead1b70ef26e47
melodyleonardv <m.oparaugo@vetifly.com>
2022-07-26 12:30:50 +0100

    added heroku api

--- src/apollo/client.ts
+++ src/apollo/client.ts
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache} from '@apollo/client';
 
 const token = localStorage.getItem('token');
 export const client = new ApolloClient({
-  uri: 'http://localhost:4000/api/v1',
+  uri: 'https://contact-api-expectoo.herokuapp.com/api/v1',
   cache: new InMemoryCache(),
   headers:{
      authorization: token ? `Bearer ${token}` : '',
